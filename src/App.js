c569e1d77a96ebe9081d945529a1456f6eaddc0e
Matt Morris <matt@Matts-MacBook-Pro.local>
2020-05-03 13:12:08 -0400

    Core functionality working.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Alert from 'react-bootstrap/Alert'
 import Button from 'react-bootstrap/Button'
 import Card from 'react-bootstrap/Card'
 import CardDeck from 'react-bootstrap/CardDeck'
+import CardGroup from 'react-bootstrap/CardGroup'
 import Container from 'react-bootstrap/Container'
 import Form from 'react-bootstrap/Form'
 
+import Resizer from 'react-image-file-resizer';
+
 import 'bootstrap/dist/css/bootstrap.min.css'
+
+function dataURItoFile(dataURI) {
+    var byteString;
+    if (dataURI.split(',')[0].indexOf('base64') >= 0)
+        byteString = atob(dataURI.split(',')[1]);
+    else
+        byteString = unescape(dataURI.split(',')[1]);
+
+    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+    var ia = new Uint8Array(byteString.length);
+    for (var i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
+    }
+
+    var blob = new Blob([ia], {type:mimeString});
+
+    return new File([blob], "file", { type: blob.type });
+}
 
 function App() {
   const [description, setDescription] = useState();
-  const [files, setFiles] = useState();
+  const [file, setFile] = useState();
+  const [inspections, setInspections] = useState([]);
+  const [hasSubmitted, setHasSubmitted] = useState(false);
+  const [uploadSuccess, setUploadSuccess] = useState(false);
+  const [validationError, setValidationError] = useState(false);
+
+  useEffect(() => {
+    if (inspections.length === 0) {
+      getInspections();
+    }
+  });
+
+  const fileChangedHandler = (event) => {
+      var fileInput = false
+      if(event.target.files[0]) {
+          fileInput = true;
+      }
+      if(fileInput) {
+          Resizer.imageFileResizer(
+              event.target.files[0],
+              300,
+              300,
+              'JPEG',
+              50,
+              0,
+              uri => {
+                var file = dataURItoFile(uri)
+                setFile(file)
+              },
+              'base64'
+          );
+      }
+  }
 
   const upload = (e) => {
     e.preventDefault();
 
-    if (description && files) {
+    console.log(inspections);
+
+    if (description && file) {
+      setValidationError(false);
+
       var formData = new FormData();
-      formData.append(`file`, files[0]);
+      formData.append(`file`, file);
       formData.append('description', description);
 
       fetch('http://74.208.182.244/inspection/upload', {
@@ -27,36 +85,57 @@ function App() {
       }, {mode: 'cors'})
         .then(response => response.json())
         .then(success => {
-          // Do something with the successful response
+          getInspections();
+          setUploadSuccess(true);
+          setHasSubmitted(true);
+          setDescription("");
+          setFile("");
         })
-        .catch(error => console.log(error)
-      );
+        .catch(error => {
+            setUploadSuccess(false);
+            setHasSubmitted(true);
+          }
+        )
+
+      return
     }
+
+    setValidationError(true);
+  }
+
+  const getInspections = () => {
+    fetch('http://74.208.182.244/inspection/all', {
+      method: 'GET'
+    }, {mode: 'cors'})
+      .then(response => {return response.json()})
+      .then(json => { setInspections(json.inspections); })
+      .then(success => {
+      })
+      .catch(error => console.log(error)
+    );
   }
 
   return (
     <Container fluid style={{padding: '20px'}}>
-      <Alert variant="success" style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
+      { hasSubmitted ?
+      <Alert variant={uploadSuccess ? `success` : `danger`} style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
         Inspection creation was successful.
-        <Button variant="outline-success">
+        <Button variant={uploadSuccess ? `outline-success` : `outline-danger`} onClick={() => setHasSubmitted(false)}>
           Close
         </Button>
-      </Alert>
-      <Alert variant="danger" style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
-        Inspection creation has failed.
-        <Button variant="outline-danger">
-          Try Again
-        </Button>
-      </Alert>
+      </Alert> : <></>
+      }
       <Card>
         <Card.Body>
           <h3>New Inspection</h3>
+          { validationError ? <h5 style={{ color: 'red' }}>Description and a file are both required</h5> : <></> }
           <Form>
             <Form.Group>
               <Form.Label>Description</Form.Label>
               <Form.Control
               as="textarea"
               rows="3"
+              value={description}
               placeholder="Details about the inspection"
               onChange={(event) => setDescription(event.target.value)}
               />
@@ -66,7 +145,7 @@ function App() {
               <Form.File
                 id="custom-file"
                 label="Custom file input"
-                onChange={(event) => setFiles(event.target.files)}
+                onChange={(event) => fileChangedHandler(event)}
                 custom
               />
             </Form.Group>
@@ -78,48 +157,24 @@ function App() {
       </Card>
       <Container fluid style={{marginTop: '20px'}}>
         <h3>Inspections</h3>
+        { inspections.length ?
         <CardDeck>
-          <Card>
-            <Card.Img variant="top" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22296%22%20height%3D%22160%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20296%20160%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_171d6f0364b%20text%20%7B%20fill%3A%23999%3Bfont-weight%3Anormal%3Bfont-family%3A-apple-system%2CBlinkMacSystemFont%2C%26quot%3BSegoe%20UI%26quot%3B%2CRoboto%2C%26quot%3BHelvetica%20Neue%26quot%3B%2CArial%2C%26quot%3BNoto%20Sans%26quot%3B%2Csans-serif%2C%26quot%3BApple%20Color%20Emoji%26quot%3B%2C%26quot%3BSegoe%20UI%20Emoji%26quot%3B%2C%26quot%3BSegoe%20UI%20Symbol%26quot%3B%2C%26quot%3BNoto%20Color%20Emoji%26quot%3B%2C%20monospace%3Bfont-size%3A15pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_171d6f0364b%22%3E%3Crect%20width%3D%22296%22%20height%3D%22160%22%20fill%3D%22%23373940%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22108.5078125%22%20y%3D%2287.05%22%3E296x160%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" />
-            <Card.Body>
-              <Card.Title>Card title</Card.Title>
-              <Card.Text>
-                This is a wider card with supporting text below as a natural lead-in to
-                additional content. This content is a little bit longer.
-              </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </Card.Footer>
-          </Card>
-          <Card>
-            <Card.Img variant="top" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22296%22%20height%3D%22160%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20296%20160%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_171d6f0364b%20text%20%7B%20fill%3A%23999%3Bfont-weight%3Anormal%3Bfont-family%3A-apple-system%2CBlinkMacSystemFont%2C%26quot%3BSegoe%20UI%26quot%3B%2CRoboto%2C%26quot%3BHelvetica%20Neue%26quot%3B%2CArial%2C%26quot%3BNoto%20Sans%26quot%3B%2Csans-serif%2C%26quot%3BApple%20Color%20Emoji%26quot%3B%2C%26quot%3BSegoe%20UI%20Emoji%26quot%3B%2C%26quot%3BSegoe%20UI%20Symbol%26quot%3B%2C%26quot%3BNoto%20Color%20Emoji%26quot%3B%2C%20monospace%3Bfont-size%3A15pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_171d6f0364b%22%3E%3Crect%20width%3D%22296%22%20height%3D%22160%22%20fill%3D%22%23373940%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22108.5078125%22%20y%3D%2287.05%22%3E296x160%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" />
-            <Card.Body>
-              <Card.Title>Card title</Card.Title>
-              <Card.Text>
-                This card has supporting text below as a natural lead-in to additional
-                content.{' '}
-              </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </Card.Footer>
-          </Card>
-          <Card>
-            <Card.Img variant="top" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22296%22%20height%3D%22160%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20296%20160%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_171d6f0364b%20text%20%7B%20fill%3A%23999%3Bfont-weight%3Anormal%3Bfont-family%3A-apple-system%2CBlinkMacSystemFont%2C%26quot%3BSegoe%20UI%26quot%3B%2CRoboto%2C%26quot%3BHelvetica%20Neue%26quot%3B%2CArial%2C%26quot%3BNoto%20Sans%26quot%3B%2Csans-serif%2C%26quot%3BApple%20Color%20Emoji%26quot%3B%2C%26quot%3BSegoe%20UI%20Emoji%26quot%3B%2C%26quot%3BSegoe%20UI%20Symbol%26quot%3B%2C%26quot%3BNoto%20Color%20Emoji%26quot%3B%2C%20monospace%3Bfont-size%3A15pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_171d6f0364b%22%3E%3Crect%20width%3D%22296%22%20height%3D%22160%22%20fill%3D%22%23373940%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22108.5078125%22%20y%3D%2287.05%22%3E296x160%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" />
-            <Card.Body>
-              <Card.Title>Card title</Card.Title>
-              <Card.Text>
-                This is a wider card with supporting text below as a natural lead-in to
-                additional content. This card has even longer content than the first to
-                show that equal height action.
-              </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </Card.Footer>
-          </Card>
+          { inspections.map((inspection) => (
+          <CardGroup style={{ marginTop: '20px' }}>
+            <Card key={inspection._id}>
+              <Card.Img variant="top" src={inspection.path} />
+              <Card.Body>
+                <Card.Title>ID: {inspection._id}</Card.Title>
+                <Card.Text>{inspection.description}</Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                <small className="text-muted">Last updated 3 mins ago</small>
+              </Card.Footer>
+            </Card>
+          </CardGroup>
+          ))}
         </CardDeck>
+        : <>No Inspections</>}
       </Container>
     </Container>
   );
